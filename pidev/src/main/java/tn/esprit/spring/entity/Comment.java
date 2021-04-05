cbbf60983ba28dd7d488742578d41dee6a01ede4
mohamedsouguir <78762135+mohamedsouguir@users.noreply.github.com>
2021-04-05 18:04:05 +0100

    working pi

---
+++ pidev/src/main/java/tn/esprit/spring/entity/Comment.java
@@ -0,0 +1,130 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="comment")
+public class Comment implements Serializable {
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_comment")
+	private Long id;
+	@Column
+	private String description;
+	@Column
+	private Date date;
+	
+
+	@ManyToOne(cascade= CascadeType.ALL)
+	@JoinColumn(name="Post_commt")
+	private Post post;
+	
+	
+	
+	public Comment(Long id, String description, Date date) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.date = date;
+	
+	}
+	
+	public Comment() {
+		
+	}
+	
+	
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Temporal(TemporalType.DATE)
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Comment [description=" + description + ", date=" + date + ", getDescription()=" + getDescription()
+				+ ", getDate()=" + getDate() + "]";
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
