d243aa02b17907b98ce40f9f39b5717ae09f5d49
mohamedsouguir <78762135+mohamedsouguir@users.noreply.github.com>
2021-04-09 19:19:01 +0100

    feragh

--- pidev/src/main/java/tn/esprit/spring/repository/ParentRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.esprit.spring.entity.Parent;
-
-public interface ParentRepository extends JpaRepository<Parent, Long>{
-
-}
